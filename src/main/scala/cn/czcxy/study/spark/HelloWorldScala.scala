a23e802ec052930dca9a51250f78d84e861a158f
王伟华 <kt_wangweihua@tcl.com>
2019-12-27 16:31:21 +0800

    scala

---
+++ src/main/scala/cn/czcxy/study/spark/HelloWorldScala.scala
@@ -0,0 +1,9 @@
+package cn.czcxy.study.spark
+
+object HelloWorldScala {
+  def main(args: Array[String]): Unit = {
+    var s: String = "hello world"
+    print(s)
+    println("hello world scala")
+  }
+}
