83c8992c15c4ac7c95da07535294bf89685d1b4a
王伟华 <979982272@qq.com>
2020-02-21 11:41:28 +0800

    修改日志级别

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hadoopStudy
 hadoop学习
+scala 2.12.10
+spark 2.4.4
+运行sparkstreaming需要启动hadoop
+并且配置 本地ip映射172.17.160.179->47.93.187.183
--- src/main/scala/cn/czcxy/study/sparkstearm/SparkStreamStudy.scala
+++ src/main/scala/cn/czcxy/study/sparkstearm/SparkStreamStudy.scala
@@ -1,21 +1,20 @@
 package cn.czcxy.study.sparkstearm
 
+import org.apache.hadoop.conf.Configuration
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.InputDStream
-import org.apache.spark.streaming.kafka010
+import org.apache.spark.deploy.SparkHadoopUtil
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.log4j.{Level, Logger}
 
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 
 /**
   * sparkstreaming 学习，消费kafka消息
   */
 object SparkStreamStudy {
-  val CHECK_POING_PATH = "E://spark/sparkstreaming/checkpoint_sparkstreamingstudy/"
+  val CHECK_POING_PATH = "hdfs://aliyun:9000//spark/sparkstreaming/checkpoint_sparkstreamingstudy/"
 
   /**
     * 处理来自kafka的消息
@@ -27,8 +26,6 @@ object SparkStreamStudy {
       * TODO 2. 创建kafka链接
       */
     // kafka配置信息
-  //  val kafkaParms: Map[String, Object] = Map("metadata.broker.list" -> " 47.93.187.183:9092")
-
     val kafkaParms = Map[String, Object](
       "bootstrap.servers" -> "47.93.187.183:9092",
       "key.deserializer" -> classOf[StringDeserializer],
@@ -38,33 +35,25 @@ object SparkStreamStudy {
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
-    // kafka中对应的topic
-    val topic: Set[String] = Set("order-topic")
-    // 通过createDirectStream构建
-    //    val kafkaDStream: InputDStream[(String, String)] = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParms, topic)
-    //    kafkaDStream.transform((rdd, time) => {
-    //      print(1)
-    //      null
-    //    })
-
     val map = scala.collection.mutable.Map[TopicPartition, String]()
     map.put(new TopicPartition("order-topic", 0), "s102")
-    val locStra: LocationStrategy = LocationStrategies.PreferFixed(map)
+    val locationStrategy: LocationStrategy = LocationStrategies.PreferFixed(map)
 
     val topicPartitions = scala.collection.mutable.ArrayBuffer[TopicPartition]()
     topicPartitions.+=(new TopicPartition("order-topic", 0))
-    val value: ConsumerStrategy[String, String] = ConsumerStrategies.Assign[String, String](topicPartitions, kafkaParms)
+    val consumerStrategy: ConsumerStrategy[String, String] = ConsumerStrategies.Assign[String, String](topicPartitions, kafkaParms)
     //创建kakfa直向流
     val kafkaDStream = KafkaUtils.createDirectStream[String, String](
       ssc,
-      locStra,
-      value
+      locationStrategy,
+      consumerStrategy
     )
     kafkaDStream.transform((rdd, time) => {
       rdd
         .mapPartitions(iter => {
           // 针对每个分区数据操作： TODO：RDD中每个分区数据对应于Kafka Topic中每个分区的数据
           iter.map(item => {
+            print(item.value())
             val array = item.value().split(",")
             // 返回二元组，按照省份ID进行统计订单销售额，所以省份ID为Key
             (array(0), array(1))
@@ -75,6 +64,13 @@ object SparkStreamStudy {
   }
 
   def main(args: Array[String]): Unit = {
+    // Logger.getRootLogger.setLevel(Level.WARN)
+
+
+    // 设置hadoop配置信息
+    val hadoopConf: Configuration = SparkHadoopUtil.get.conf
+    // 使用域名访问hadoop而不是ip
+    hadoopConf.set("dfs.client.use.datanode.hostname", "true")
     // TODO 1. 创建StreamingContext对象
     var cxt: StreamingContext = StreamingContext.getOrCreate(CHECK_POING_PATH, () => {
       // 创建spark基础配置信息
@@ -83,13 +79,15 @@ object SparkStreamStudy {
         .setAppName("MapWithStateKafkaStreaming")
       val ssc = new StreamingContext(conf, Seconds(5))
       // 设置日志级别
-      ssc.sparkContext.setLogLevel("WARN")
+     ssc.sparkContext.setLogLevel("DEBUG")
       // 设置检查点信息
-      //  ssc.checkpoint(CHECK_POING_PATH)
+      ssc.checkpoint(CHECK_POING_PATH)
       // 处理kafka信息
       processData(ssc)
       ssc
-    })
+    }, hadoopConf)
+   // cxt.sparkContext.setLogLevel("ERROR")
+   // cxt.sparkContext.setLocalProperty("log4j.rootCategory", "ERROR, console")
     // TODO 2. 启动应用
     cxt.start()
     cxt.awaitTermination()
