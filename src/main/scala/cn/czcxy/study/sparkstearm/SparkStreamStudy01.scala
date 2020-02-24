667ef97efd3025d2e99e77f9b8bac94274ce3f02
王伟华 <kt_wangweihua@tcl.com>
2020-02-24 16:05:01 +0800

    aaa

--- src/main/scala/cn/czcxy/study/sparkstearm/SparkStreamStudy01.scala
+++ src/main/scala/cn/czcxy/study/sparkstearm/SparkStreamStudy01.scala
@@ -7,7 +7,7 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 /**
   *sparkstreaming 简单程序，链接 47.93.187.183 7777 的socket端口，在47.93.187.183 安装nc
   * yum install nc
-  *  nc -lk 7777 启动应用 ，在控制台 输入 这边就能收到 -lk(小写LK)
+  *  nc -lk 7777 启动应用 ，在控制台 输入 这边就能收到 -lk(小写LK)   aaa
   */
 object SparkStreamStudy01 {
   def main(args: Array[String]): Unit = {
